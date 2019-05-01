2df31baab092767b023bc633a291cb0943df0098
Jason Leach <jlspursfan@gmail.com>
2019-05-01 16:25:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(arr, ele) {
-  [ele, ...arr]
+  return [ele, ...arr]
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, ele) {
