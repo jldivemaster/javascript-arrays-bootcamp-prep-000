8f07a1d811a25fc31ae157603368fe0345f101ba
Jason Leach <jlspursfan@gmail.com>
2019-05-01 15:57:09 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+function addElementToBeginningOfArray(arr, ele) {
+  [ele, ...arr]
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, ele) {
+  arr.unshift(ele)
+}
+
+function addElementToEndOfArray() {
+  
+}
